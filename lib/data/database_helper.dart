9e22d55b3d347853cf697423e46ff56cf710258e
Kemal Gokkaya <kemalgokkaya8@gmail.com>
2025-10-31 19:28:36 +0300

    added Sqflite files

---
+++ lib/data/database_helper.dart
@@ -0,0 +1,34 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB("notes.db");
+    return _database!;
+  }
+
+  Future<Database> _initDB(String fileName) async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = join(directory.path, fileName);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE notes(
+        id TEXT PRIMARY KEY,
+        title TEXT,
+        content TEXT
+      )
+    ''');
+  }
+}
